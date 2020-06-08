3b6de4d68bf8cdb181d2723f7d83d81503e21507
Bill Njoroge <williamsriunge@gmail.com>
2020-06-08 10:19:46 +0300

    Frst COmmit

--- send.py
+++ send.py
@@ -1,5 +1,6 @@
-from datetime import datetime
+import maya
 import pyinputplus as pypi
+
 user_name = pypi.inputStr("Enter your name please: ")   
 
 user_email = pypi.inputEmail("Enter your email please: ").lower()
@@ -8,8 +9,10 @@ password = pypi.inputPassword("""\n Your email password is needed for authentica
 Enter your email password: """)
 name_input = pypi.inputStr("Enter the name of the person you wish to send the email: ")
 email_input = pypi.inputEmail("Enter his/her email as well: ").lower()
-
+subject = pypi.inputStr("What is the Title of your email? \n")
+#date_sent = pypi.inputDatetime(" WHen do you want to send teh email? ")
 def send_msg():
-        welcome_message = """ Hi {0}
-        Welcome to {1}. We are so happy to have you""" .format(name_input, "instagram.com")
+        welcome_message = pypi.inputStr('''Enter your message: \n
+        ''')
+        print(maya.now())
         return welcome_message
--- sendemail.py
+++ sendemail.py
@@ -5,29 +5,31 @@ from send import *
 import pyinputplus as pypi
 
 #Main function that sends the mail
-def send_mail(text = send_msg(), subject = "Welcome!", from_email = user_email , to_email = [email_input], html = None):
-    assert isinstance(to_email, list)                            #Check if it's a string otherwie, return assertion error
-    msg = MIMEMultipart("alternative")
-    msg["From"] = from_email
-    msg["To"] = "," .join(to_email)
-    msg["Subject"] = subject
-    text_part = MIMEText(text, "plain")
-    msg.attach(text_part)
-    if html != None:
-        html_part = MIMEText("<h1> This is my first Email!</h1>")
-        msg.attach(html_part)
+def send_mail(text = send_msg(), subject = subject, 
+    from_email = user_email , to_email = [email_input],
+    html = None):
+    while True:
+        assert isinstance(to_email, list)                            #Check if it's a string otherwie, return assertion error
+        msg = MIMEMultipart("alternative")
+        msg["From"] = from_email
+        msg["To"] = "," .join(to_email)
+        msg["Subject"] = subject
+        text_part = MIMEText(text, "plain")
+        msg.attach(text_part)
+        if html != None:
+            html_part = MIMEText("<h1> This is my first Email!</h1>")
+            msg.attach(html_part)
+            
+        msg_str = msg.as_string()
         
-    msg_str = msg.as_string()
-    
-    #Login to the STMP Servers
+        #Login to the STMP Servers
 
-    with smtplib.SMTP(host = "smtp.gmail.com", port = 587) as server:
-        server.ehlo()
-        server.starttls()
-        server.login(user_email, password)
-        server.sendmail(from_email, to_email, msg_str)
-
-
+        with smtplib.SMTP(host = "smtp.gmail.com", port = 587) as server:
+            server.ehlo()
+            server.starttls()
+            server.login(user_email, password)
+            server.sendmail(from_email, to_email, msg_str)
+            break
 send_mail()
     
     
